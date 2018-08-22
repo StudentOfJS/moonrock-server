37ba6b46e44fb33b99091d0bced4f39fbb115838
StudentOfJS <rodlewis45@gmail.com>
2018-08-22 18:03:07 +0200

    create structs for handlers

--- handlers.go
+++ handlers.go
@@ -1,6 +1,29 @@
 package main
 
-type Newsletter struct {
+// NewsletterSignup is used for the newsletter signup
+type NewsletterSignup struct {
 	Email string `json:"email"`
-	First bool   `json:"first"`
+}
+
+// Newsletter stores details for sending emails
+type NewsletterData struct {
+	Allowed bool              `json:"allowed"`
+	Email   *NewsletterSignup `json:"email"`
+	First   bool              `json:"first_email"`
+	Last    int16             `json:"last_sent"`
+}
+
+// Login is for basic username and password login - may swap for SSO
+type Login struct {
+	Password string `json:"password"`
+	Username string `json:"email"`
+}
+
+// User is encompases all held user data
+type User struct {
+	Ethereum       string          `json:"ethaddress"`
+	FirstName      string          `json:"first_name"`
+	LastName       string          `json:"last_name"`
+	Login          *Login          `json:"login_details"`
+	NewsletterData *NewsletterData `json:"newsletter-data"`
 }
