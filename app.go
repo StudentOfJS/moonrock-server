f9d044168c50b8b9edd4da56a34ad3897289ae54
StudentOfJS <rodlewis45@gmail.com>
2018-08-22 17:28:55 +0200

    add route for newsletter signup

--- app.go
+++ app.go
@@ -11,22 +11,13 @@ import (
 var cache redis.Conn
 
 func main() {
+	// Init Router
 	r := mux.NewRouter()
-	r.HandleFunc("/signin", Signin)
-	r.HandleFunc("/welcome", Welcome)
-	r.HandleFunc("/refresh", Refresh)
-	r.HandleFunc("/signup", Signup)
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/newsletter", Newsletter).Methods("POST")
 
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func initCache() {
-	conn, err := redis.DialURL("redis://localhost")
-	if err != nil {
-		panic(err)
-	}
-
-	cache = conn
 }
