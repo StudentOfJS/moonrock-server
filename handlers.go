39f0bff4f113f9b1e0a52fae6844889ec54f5dfc
StudentOfJS <rodlewis45@gmail.com>
2018-08-23 14:29:58 +0200

    Newsletter func make more readable

--- handlers.go
+++ handlers.go
@@ -39,11 +39,12 @@ type User struct {
 
 // Newsletter - signs up from PUT request with email to newsletter
 func Newsletter(c *gin.Context) {
-	if c.PostForm("email") == "" {
+	email := c.PostForm("email")
+	if email == "" {
 		c.String(200, "no email provided")
 	}
 	Db.Update(func(tx *bolt.Tx) error {
-		u2, err := uuid.FromString(c.PostForm("email"))
+		u2, err := uuid.FromString(email)
 		if err != nil {
 			return fmt.Errorf("uuid went wrong: %s", err)
 		}
@@ -63,7 +64,7 @@ func Newsletter(c *gin.Context) {
 			c.String(200, "error  creating user bucket")
 			return fmt.Errorf("userBucket: %s", err)
 		}
-		err = ub.Put([]byte("email"), []byte(c.PostForm("email")))
+		err = ub.Put([]byte("email"), []byte(email))
 		if err != nil {
 			c.String(200, "error writing email")
 			return fmt.Errorf("create email: %s", err)
@@ -83,33 +84,4 @@ func Newsletter(c *gin.Context) {
 	})
 
 	c.String(200, "ok")
-}
-
-func Put(c *gin.Context) {
-
-	if c.PostForm("bucket") == "" || c.PostForm("key") == "" {
-		c.String(200, "no bucket name or key | n")
-	}
-
-	Db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte(c.PostForm("bucket")))
-		if err != nil {
-
-			c.String(200, "error  creating bucket | n")
-			return fmt.Errorf("create bucket: %s", err)
-		}
-
-		err = b.Put([]byte(c.PostForm("key")), []byte(c.PostForm("value")))
-
-		if err != nil {
-
-			c.String(200, "error writing KV | n")
-			return fmt.Errorf("create kv: %s", err)
-		}
-
-		return nil
-	})
-
-	c.String(200, "ok")
-
 }
