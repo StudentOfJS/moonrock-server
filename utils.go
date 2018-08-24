cfe8e00fb5fb310e48d8cd41c995002158ecc907
StudentOfJS <rodlewis45@gmail.com>
2018-08-24 15:45:35 +0200

    update structs

--- utils.go
+++ utils.go
@@ -10,14 +10,14 @@ type EmailTest struct {
 	Email string `validate:"min=5,max=255,regexp=^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$"`
 }
 
-// LoginDetails contains validation for login details
-type LoginDetails struct {
+// LoginTest contains validation for login details
+type LoginTest struct {
 	Username string `validate:"min=5,max=255,regexp=^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$"`
 	Password string `validate:"min=8",max=255`
 }
 
-// UserDetails contains validation for user details
-type UserDetails struct {
+// UserTest contains validation for user details
+type UserTest struct {
 	Ethereum  string `validate"regexp=^0x[a-fA-F0-9]{40}$"`
 	FirstName string `validate:"min=1",max=255`
 	LastName  string `validate:"min=1",max=255`
@@ -25,7 +25,7 @@ type UserDetails struct {
 
 // LoginNotValid returns true if validation fails for username or password
 func LoginNotValid(username string, password string) bool {
-	loginRequest := LoginDetails{Username: username, Password: password}
+	loginRequest := LoginTest{Username: username, Password: password}
 	if errs := validator.Validate(loginRequest); errs != nil {
 		return true
 	}
@@ -34,7 +34,7 @@ func LoginNotValid(username string, password string) bool {
 
 // UserNotValid returns true if validation fails for user details
 func UserNotValid(e string, f string, l string) bool {
-	signupRequest := UserDetails{Ethereum: e, FirstName: f, LastName: f}
+	signupRequest := UserTest{Ethereum: e, FirstName: f, LastName: f}
 	if errs := validator.Validate(signupRequest); errs != nil {
 		return true
 	}
