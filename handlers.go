906c8d030c548008b651a37b773e49e7e3450919
StudentOfJS <rodlewis45@gmail.com>
2018-08-28 10:29:34 +0200

    move automailer to handlers

--- handlers.go
+++ handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"strconv"
+	"time"
 
 	"github.com/asdine/storm"
 	"github.com/gin-gonic/gin"
@@ -244,6 +246,44 @@ func ResetPasswordHandler(c *gin.Context) {
 		db.UpdateField(&User{ResetCode: rc}, "ResetCode", newResetCode)
 		c.JSON(200, gin.H{"status": "success"})
 	}
+}
+
+// sendTenWelcomeMails gets up to 10 new subscriptions and sends them each a welcome email
+func sendTenWelcomeMails(done chan bool) {
+	var receivers []Subscription
+	// Start boltDB
+	db, err := storm.Open("my.db")
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	defer db.Close()
+
+	err = db.Find("Confirmed", false, &receivers, storm.Limit(10))
+	if err != nil {
+		log.Panic(err)
+	}
+	subject := "Moonrock ICO Confirmation"
+
+	for _, receiver := range receivers {
+		r := NewRequest([]string{receiver.Email}, subject)
+		r.Send("templates/welcome_template.html", map[string]string{"username": "Welcome"})
+		err := db.UpdateField(&Subscription{NewsLetterID: receiver.NewsLetterID}, "Confirmation", true)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+	done <- true
+}
+
+// SendWelcomeEmails checks for new subscriptions twice a day and sends
+func SendWelcomeEmails() {
+	done := make(chan bool, 1)
+	go sendTenWelcomeMails(done)
+	<-done
+	timer := time.NewTimer(12 * time.Hour)
+	<-timer.C
+	SendWelcomeEmails()
 }
 
 // TokenSaleUpdatesHandler - signs up from PUT request with email to newsletter
