ab908e588e08d11977a687fb36b1fdeaf02004f4
StudentOfJS <rodlewis45@gmail.com>
2018-08-31 15:55:00 +0200

    add db test

--- moonrock_test.go
+++ moonrock_test.go
@@ -3,14 +3,17 @@ package main
 import (
 	"reflect"
 	"testing"
+
+	"github.com/asdine/storm"
 )
 
-var e string = "test@test.com.au"
+var e = "test@test.com.au"
 
+/* ------------------- utils ------------------------- */
 func TestHashPassword(t *testing.T) {
 	hash, err := HashPassword(Password)
 	if err != nil {
-		t.Errorf("Expected a hash but recieved an error: %d", err)
+		t.Errorf("Expected a hash but recieved an error: %s", err)
 	} else {
 		hashType := reflect.TypeOf(hash).String()
 		if hashType != "[]uint8" {
@@ -21,7 +24,7 @@ func TestHashPassword(t *testing.T) {
 
 func TestLoginValid(t *testing.T) {
 	if err := LoginValid(Username, Password); err != nil {
-		t.Errorf("Provided valid username and password got: %d", err)
+		t.Errorf("Provided valid username and password got: %s", err)
 	}
 	if err := LoginValid(Username, "x"); err == nil {
 		t.Error("Provided invalid password, expected login check to fail, but it passed")
@@ -34,7 +37,7 @@ func TestLoginValid(t *testing.T) {
 func TestUserValid(t *testing.T) {
 	e, f, l := "0xCaE9eFE97895EF43e72791a10254d6abDdb17Ae9", "Rod", "Lewis"
 	if err := UserValid(e, f, l); err != nil {
-		t.Errorf("Provided valid user details, but recieved: %d", err)
+		t.Errorf("Provided valid user details, but recieved: %s", err)
 	}
 	if err := UserValid("not_valid", f, l); err == nil {
 		t.Error("Provided invalid eth address, but check passed")
@@ -46,7 +49,7 @@ func TestUserValid(t *testing.T) {
 
 func TestEmailValid(t *testing.T) {
 	if err := EmailValid(e); err != nil {
-		t.Errorf("Provided valid email, but recieved: %d", err)
+		t.Errorf("Provided valid email, but recieved: %s", err)
 	}
 }
 
@@ -54,7 +57,7 @@ func TestCreateUUID(t *testing.T) {
 	s := "F0001234-0451-4000-B000-000000000000"
 	id, err := CreateUUID(s)
 	if err != nil {
-		t.Errorf("Provided valid string, expected id, but recieved: %d", err)
+		t.Errorf("Provided valid string, expected id, but recieved: %s", err)
 	} else {
 		if reflect.TypeOf(id).String() != "uuid.UUID" {
 			t.Error("Expected type of id to be uuid.UUID")
@@ -70,6 +73,18 @@ func TestLoginCheck(t *testing.T) {
 		t.Error("Provided invalid login details, expected error, but recieved nil")
 	}
 	if err := LoginCheck(TestUser, TestPass); err.Error() != "confirm email" {
-		t.Errorf("Provided valid login details, but recieved: %d", err)
+		t.Errorf("Provided valid login details, but recieved: %s", err)
+	}
+}
+
+/* ------------------- Database ------------------------- */
+
+func TestDB(t *testing.T) {
+	// Start boltDB
+	db, err := storm.Open("my.db")
+	defer db.Close()
+	if err != nil {
+		t.Errorf("Database failed to open: %v", err)
+		return
 	}
 }
