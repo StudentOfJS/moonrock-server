b3232ac85f3bccbd1499f078f4e9c4ebe8c316a6
StudentOfJS <rodlewis45@gmail.com>
2018-09-06 17:17:45 +0200

    add test for getResponse

---
+++ models/response_test.go
@@ -0,0 +1,34 @@
+package models
+
+import "testing"
+
+type testResponses struct {
+	responses  []string
+	serverCode int
+}
+
+var tests = []testResponses{
+	{[]string{"updated", "check your email", "ok", "account successfully confirmed"}, 200},
+	{[]string{
+		"invalid login",
+		"invalid email",
+		"invalid signup",
+		"user doesn't exist",
+		"token expired, please try again",
+		"update failed",
+		"already signed up",
+	}, 400},
+	{[]string{"unauthenticated"}, 401},
+	{[]string{"server error"}, 500},
+}
+
+func TestgetResponse(t *testing.T) {
+	for _, pair := range tests {
+		for _, test := range pair.responses {
+			code := getResponse(test)
+			if code.serverCode != pair.serverCode {
+				t.Fail()
+			}
+		}
+	}
+}
