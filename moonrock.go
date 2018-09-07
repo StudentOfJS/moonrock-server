5695c28da16ac766df458051e754e51eb12d187d
StudentOfJS <rodlewis45@gmail.com>
2018-09-07 16:41:19 +0200

    init func with db credential setup

--- moonrock.go
+++ moonrock.go
@@ -5,7 +5,9 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	db "github.com/studentofjs/moonrock-server/database"
+	"github.com/studentofjs/moonrock-server/database"
+	"github.com/studentofjs/moonrock-server/models"
+	"github.com/studentofjs/moonrock-server/secrets"
 	cors "gopkg.in/gin-contrib/cors.v1"
 )
 
@@ -18,8 +20,41 @@ func apiRouter() {
 	log.Fatal(r.Run(":4000")) // log server error
 }
 
+func init() {
+	db, err := database.OpenDB()
+	if err != nil {
+		return getResponse("server error")
+	}
+	defer db.Close()
+
+	hash, err := models.HashPassword(secrets.ClientSecret)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	clientCredentials := models.User{
+		Group:    "client",
+		Password: hash,
+		Username: secrets.ClientID,
+	}
+
+	db.Save(&clientCredentials)
+
+	hash, err = models.HashPassword(secrets.TestPass)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	clientCredentials = models.User{
+		Group:    "testing",
+		Password: hash,
+		Username: secrets.TestUser,
+	}
+	db.Save(&clientCredentials)
+}
+
 func main() {
 	fmt.Println("Rest API v1.0")
-	db.HandleDB()
 	apiRouter()
 }
