8e9aa426987e4d741e62b4b42f59c9e0b3265bf9
StudentOfJS <rodlewis45@gmail.com>
2018-09-08 11:05:45 +0200

    create mock data for registering users

--- models/users_test.go
+++ models/users_test.go
@@ -1,54 +1,69 @@
 package models
 
-type testLogin struct {
-	username string
-	password string
-	valid    bool
+
+var testUsers = []User{
+  {
+    Address:         "1 Chester Field Green, Baltimore Fields, Baltimore, MA",
+    Confirmed:       false,
+    CountryCode:     "US",
+    EthereumAddress: "0xe81D72D14B1516e68ac3190a46C93302Cc8eD60f",
+    FirstName:       "Teddy",
+    Group:           "public_investor",
+    LastName:        "Weinstein",
+    Password:        "TotalMayhem",
+    ResetCode:       resetcode,
+    Username:        "teddy.w@test.com",
+  },
+  {
+    Address:         "12 Bacon Court, Saltash, Cornwall, UK",
+    Confirmed:       false,
+    CountryCode:     "UK",
+    EthereumAddress: "0x6a068E0287e55149a2a8396cbC99578f9Ad16A31",
+    FirstName:       "Dave",
+    Group:           "public_investor",
+    LastName:        "Saville",
+    Password:        "Loser",
+    ResetCode:       resetcode,
+    Username:        "dave@test.com",
+  },
+  {
+    Address:         "82 Avalon Plains, Esperance, WA, Australia",
+    Confirmed:       false,
+    CountryCode:     "AU",
+    EthereumAddress: "0xe81D72D14B1516e68ac3190a46C93302Cc8eE60c",
+    FirstName:       "Brad",
+    Group:           "public_investor",
+    LastName:        "Tad",
+    Password:        "SurfOrDie2",
+    ResetCode:       resetcode,
+    Username:        "tad@test.com",
+  },
+  {
+    Address:         "8 Tornado Alley, Aliceville, Wisconsin",
+    Confirmed:       false,
+    CountryCode:     "US",
+    EthereumAddress: "0xe81D72D14B1516e68ac3190a46C93302Cc8eD60f",
+    FirstName:       "Avril",
+    Group:           "public_investor",
+    LastName:        "Smith",
+    Password:        "fhweuhwriwe34",
+    ResetCode:       resetcode,
+    Username:        "a.s@test.com",
+  },
+  {
+    Address:         "Fisherman's Cottage, Smugglers Cove, Turks and Cacos",
+    Confirmed:       false,
+    CountryCode:     "TC",
+    EthereumAddress: "0x595832F8FC6BF59c85C527fEC3740A1b7a361269",
+    FirstName:       "Peter",
+    Group:           "public_investor",
+    LastName:        "Marston",
+    Password:        "r4j3ok4j50f",
+    ResetCode:       resetcode,
+    Username:        "peter@test.com",
+  },
 }
 
-type testUser struct {
-	ethereum  string
-	firstname string
-	lastname  string
-	valid     bool
-}
-
-type testEmail struct {
-	email string
-	valid bool
-}
-
-var testLogins = []testLogin{
-	{username: "test@test.com", password: "12uhuh4rf89J", valid: true},
-	{username: "test2@test.com.au", password: "21268634238432dss", valid: true},
-	{username: "test@test.co", password: "uhuhsdfs4rf89J", valid: true},
-	{username: "test@test.net.au", password: "needtobreakFree", valid: true},
-	{username: "@test.com", password: "12uhuh4rf89J", valid: false},
-	{username: "test2@testcom", password: "21268634238432dss", valid: false},
-	{username: "test@.co", password: "uhuhsdfs4rf89J", valid: false},
-	{username: "test@test.net.au", password: "n", valid: false},
-}
-
-var testUsers = []testUser{
-	{ethereum: "0xe81D72D14B1516e68ac3190a46C93302Cc8eD60f", firstname: "coin", lastname: "lancer", valid: true},
-	{ethereum: "0x595832F8FC6BF59c85C527fEC3740A1b7a361269", firstname: "Power", lastname: "Ledger", valid: true},
-	{ethereum: "0x6a068E0287e55149a2a8396cbC99578f9Ad16A31", firstname: "dave", lastname: "saville", valid: true},
-	{ethereum: "0x08511d6c42Bd247D82746c17a3EEf0Cb235f2c48", firstname: "Ben", lastname: "Georzel", valid: true},
-	{ethereum: "08511d6c42Bd247D82746c17a3EE", firstname: "terrence", lastname: "phillip", valid: false},
-	{ethereum: "0x08511d6c42Bd247D82746c17a3EEf0Cb235f2c48", firstname: "", lastname: "Morty", valid: false},
-	{ethereum: "0x08511d6c42Bd247D82746c17a3EEf0Cb235f2c48", firstname: "Rick", lastname: "", valid: false},
-}
-
-var testEmails = []testEmail{
-	{email: "test@test.com", valid: true},
-	{email: "test2@test.com.au", valid: true},
-	{email: "test@test.co", valid: true},
-	{email: "test@test.net.au", valid: true},
-	{email: "@test.com", valid: false},
-	{email: "test2@testcom", valid: false},
-	{email: "test@.co", valid: false},
-	{email: "test.com", valid: false},
-}
 
 
 // Register validates the user signup form and saves to db
