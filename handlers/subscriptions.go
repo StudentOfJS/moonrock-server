b116346d3642fbdce7fbfa5cfc11fff1bb5c8a43
StudentOfJS <rodlewis45@gmail.com>
2018-09-12 10:23:23 +0200

    signup from view works with api

--- handlers/subscriptions.go
+++ handlers/subscriptions.go
@@ -1,13 +1,23 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/studentofjs/moonrock-server/models"
 )
 
+type email struct {
+	Email string `json:"email" binding:"required"`
+}
+
 // TGENewsletterHandler - signs up from PUT request with email to newsletter
 func TGENewsletterHandler(c *gin.Context) {
-	email := c.PostForm("email")
-	code := models.TGENewsletter(email)
+	var json email
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	code := models.TGENewsletter(string(json.Email))
 	c.JSON(code.ServerCode, gin.H{"status": code.Response})
 }
